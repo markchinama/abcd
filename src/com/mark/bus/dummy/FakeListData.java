5db098d01a9a4489145ae2073160969f91de7285
mark <mark.chinama@gmail.com>
2014-03-03 17:32:13 +0800

    change layout

--- src/com/mark/bus/dummy/FakeListData.java
+++ src/com/mark/bus/dummy/FakeListData.java
@@ -7,16 +7,13 @@ import com.mark.bus.R;
 import com.mark.bus.listable.dto.ListItemDTO;
 
 public class FakeListData {
-	
-	public static List<ListItemDTO> getData(){
-		return Arrays.asList(
-				new ListItemDTO(1, R.drawable.temp_info, "整车信息"),
+
+	public static List<ListItemDTO> getData() {
+		return Arrays.asList(new ListItemDTO(1, R.drawable.temp_info, "整车信息"),
 				new ListItemDTO(2, R.drawable.temp_transmission, "车身控制"),
 				new ListItemDTO(3, R.drawable.temp_camera, "视频监控"),
-				new ListItemDTO(4, R.drawable.temp_system_monitor, "故障诊断")
-				);
+				new ListItemDTO(4, R.drawable.temp_system_monitor, "故障诊断"));
 
-		
 	}
 
 }
