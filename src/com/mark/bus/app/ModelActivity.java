5f3384bd0042e35163f5fe2425071b6ef09578c9
unknown <mark.chinama@gmail.com>
2014-03-27 00:22:22 +0800

    .

--- src/com/mark/bus/app/ModelActivity.java
+++ src/com/mark/bus/app/ModelActivity.java
@@ -4,10 +4,14 @@ import com.mark.bus.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class ModelActivity extends Activity {
+
+	private ImageButton ecomodelbutton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +22,20 @@ public class ModelActivity extends Activity {
 		Window window = getWindow();
 		WindowManager.LayoutParams wl = window.getAttributes();
 		wl.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
-		wl.alpha = 0.8f;// 设置透明度,0.0为完全透明，1.0为完全不透明
-		wl.x = -150;
-		wl.y = -150 ;
+		wl.alpha = 0.9f;// 设置透明度,0.0为完全透明，1.0为完全不透明
+		wl.x = -180;
+		wl.y = -250;
 		window.setAttributes(wl);
-		
-		
+		ecomodelbutton = (ImageButton) this.findViewById(R.id.ecomodelbutton);
+		ecomodelbutton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				finish();
+
+			}
+		});
+
 	}
 
 }
